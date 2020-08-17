d21093fbb2b747a4671cd4141842cd42d8f0cbf3
Remco van 't Veer <remco@remworks.net>
2020-08-17 18:08:40 +0200

    Release 2.0.1

--- project.clj
+++ project.clj
@@ -1,11 +1,13 @@
-(defproject ring-partial-content "2.0.0"
+(defproject ring-partial-content "2.0.1"
   :description "Partial Content middleware for ring."
   :url "https://github.com/remvee/ring-partial-content/"
 
   :license {:name "Eclipse Public License - v 1.0"
-            :url "http://opensource.org/licenses/eclipse-1.0.php"}
+            :url  "http://opensource.org/licenses/eclipse-1.0.php"}
 
   :dependencies [[org.clojure/clojure "1.10.1"]]
+  :plugins [[lein-codox "0.10.7"]]
 
-  :codox {:src-dir-uri "https://github.com/remvee/ring-partial-content/blob/master/"
-          :src-linenum-anchor-prefix "L"})
+  :codox {:output-path    "doc"
+          :source-dir-uri ""
+          :source-uri     "https://github.com/remvee/ring-partial-content/blob/{git-commit}/{filepath}#L{line}"})
