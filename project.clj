6468164fb76775ae68d2ce51c8fc8959e9b26ccf
Remco van 't Veer <remco@remworks.net>
2023-03-28 14:14:28 +0200

    Move clojure dependency to dev profile and fix cljdoc build

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject ring-partial-content "2.0.1"
   :description "Partial Content middleware for ring."
-  :url "https://github.com/remvee/ring-partial-content/"
+  :url "https://git.sr.ht/~rwv/ring-partial-content"
+  :scm {:name "git" :url "https://git.sr.ht/~rwv/ring-partial-content"}
 
   :license {:name "Eclipse Public License - v 1.0"
             :url  "http://opensource.org/licenses/eclipse-1.0.php"}
 
-  :dependencies [[org.clojure/clojure "1.10.1"]])
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.11.1"]]}})
